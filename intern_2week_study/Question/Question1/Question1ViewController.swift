639b4354b03bacbc7e35c126b59cecf57c718178
wataru0 <wataru.tv.nanao@gmail.com>
2020-08-12 12:13:20 +0900

    solve question1-1,question1-3

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -5,20 +5,35 @@ final class Question1ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var addTextButton: UIButton!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var warningLabel: UILabel!
     
     // var to store the textField's text
     var tmpText: String = ""
     
-    // process of pushing a button
+    // process of pushing a addButton
     @IBAction func pushButton() {
         // get textFeild's text
         tmpText = textField.text!
-        // clear textFeild
-        textField.text = ""
         
-        // add textView
-        textView.text += tmpText
-        textView.text += "\n"
+        if tmpText != "" {
+            // clear textFeild
+            textField.text = ""
+            
+            // add textView
+            textView.text += tmpText
+            textView.text += "\n"
+            
+            // clear warningLabel
+            warningLabel.text = ""
+        } else {
+            warningLabel.text = "文字を入力してください"
+        }
+    }
+    
+    // process of pushing a clearTextButton
+    @IBAction func pushClearTextButton() {
+        // clear textView
+        textView.text = ""
     }
     
     override func viewDidLoad() {
@@ -28,6 +43,13 @@ final class Question1ViewController: UIViewController {
         textView.text = ""
         // background color
         textView.backgroundColor = .lightGray
+        
+        // clear warningLabel
+        warningLabel.text = ""
+        
+        // change size of textViewn
+//        let width = UIScreen.main.bounds.size.width
+//        textView.frame.size = CGSize(width: width, height: 300)
     }
     
     // process of toching non-UI componet

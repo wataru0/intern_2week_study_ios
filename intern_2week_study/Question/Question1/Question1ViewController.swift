c13a423c9969543e0b5b9b48da9e8336afffc0fd
wataru0 <wataru.tv.nanao@gmail.com>
2020-08-12 11:24:46 +0900

    question1-1

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -6,7 +6,32 @@ final class Question1ViewController: UIViewController {
     @IBOutlet weak var addTextButton: UIButton!
     @IBOutlet weak var textView: UITextView!
     
+    // var to store the textField's text
+    var tmpText: String = ""
+    
+    // process of pushing a button
+    @IBAction func pushButton() {
+        // get textFeild's text
+        tmpText = textField.text!
+        // clear textFeild
+        textField.text = ""
+        
+        // add textView
+        textView.text += tmpText
+        textView.text += "\n"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // clear textView on start-up
+        textView.text = ""
+        // background color
+        textView.backgroundColor = .lightGray
+    }
+    
+    // process of toching non-UI componet
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }

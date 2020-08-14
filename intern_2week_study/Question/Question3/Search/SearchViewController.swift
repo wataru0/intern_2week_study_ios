5bcb36709a136bb27e9deebc92104b0c8c4a7850
wataru0 <wataru.tv.nanao@gmail.com>
2020-08-14 17:16:32 +0900

    make UI

--- intern_2week_study/Question/Question3/Search/SearchViewController.swift
+++ intern_2week_study/Question/Question3/Search/SearchViewController.swift
@@ -2,6 +2,13 @@ import UIKit
 
 final class SearchViewController: UIViewController {
     
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    @IBAction func pushSearchButton() {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }

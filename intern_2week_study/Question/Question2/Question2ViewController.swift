b8a2be997b3a4f3557b1a6d96b8889eb79dafcdb
wataru0 <wataru.tv.nanao@gmail.com>
2020-08-17 12:44:44 +0900

    revision question2

--- intern_2week_study/Question/Question2/Question2ViewController.swift
+++ intern_2week_study/Question/Question2/Question2ViewController.swift
@@ -9,13 +9,13 @@ final class Question2ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.dataSource = self
-        //  Identifier 設定
-        self.tableView.register(UINib(nibName: "Question2Cell", bundle: nil), forCellReuseIdentifier: "question2Cell")
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        tableView.dataSource = self
+        
+        // R.swiftを用いない，nibファイルを登録する方法
+        //self.tableView.register(UINib(nibName: "Question2Cell", bundle: nil), forCellReuseIdentifier: "question2Cell")
+
+        // R.swiftを用いた，nibファイルの読み込み
+        tableView.register(R.nib.question2Cell)
     }
 }
 
@@ -31,9 +31,7 @@ extension Question2ViewController: UITableViewDataSource {
         guard let cell = self.tableView.dequeueReusableCell(withIdentifier: "question2Cell") as? Question2Cell else {
             return Question2Cell()
         }
-        //cell.textLabel?.text = areaTexts[indexPath.row]
         
-        //let imageUrl: String = "https://placehold.jp/150x150.png"
         let imageUrl: String = "https://j-town.net/images/2017/quote-all/town20170314163320.jpg"
         cell.setCell(cellLabel: areaTexts[indexPath.row], url: imageUrl)
         
@@ -47,25 +45,14 @@ extension Question2ViewController: UITableViewDelegate {
         let alert = UIAlertController(title: areaTexts[indexPath.row], message: "alert!!", preferredStyle: .alert)
         
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_ action: UIAlertAction!) -> Void in
-            print("ok")
         }))
         
         // Alertを表示
-        present(alert, animated: true, completion: nil)
+        present(alert, animated: true)
         // セルをタップしたときのハイライトを消す
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
-    // ヘッダーの大きさ
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return CGFloat.leastNormalMagnitude
-    }
-
-    // フッターの大きさ
-    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return CGFloat.leastNormalMagnitude
-    }
-    
+        
     // セルの高さを固定
     // セルが画面外に出ると，大きさが変化しレイアウトが崩れたため．
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
